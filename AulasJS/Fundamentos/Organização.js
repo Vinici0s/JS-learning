a2855659e3edb58a4c33c3f3bdfe70ca7ef977c7
Vincios Alburquerque <viniciusmelo.art@gmail.com>
2021-03-16 19:21:06 -0300

    refactor AulasJs

---
+++ AulasJS/Fundamentos/Organização.js
@@ -0,0 +1,33 @@
+console.log("Sentença de código")
+
+console.log("Olá Mundo")
+
+var numeral1 = 4
+numeral2 = 5
+const numeral3 = numeral1 + numeral2
+
+console.log(numeral3)
+console.log(numeral2)
+console.log(numeral1)
+
+poein = 1.5
+
+console.log(poein)
+
+const peso1 = 1.0
+const peso2 = Number('2.4')
+console.log(peso2)
+console.log(Number.isFinite(peso2))
+
+const avaliacao1 = 9.871
+const avaliacao2 = 8.745
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+console.log(total.toFixed(2))
+const media = total / (peso1 + peso2)
+
+console.log(media.toFixed(2))
+
+console.log(media.toFixed(2).toString())
+
+console.log((10).toFixed(2))
